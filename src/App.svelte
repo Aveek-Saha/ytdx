7bcc53d81253c72be9b2fb83e15a73918036cd33
Aveek Saha <aveek.s98@gmail.com>
2021-05-28 13:43:46 +0530

    Add basic layout

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,5 @@
 <script>
-    import { FormGroup, Input } from 'sveltestrap';
-
-export let name;
+    import { Progress, Input, Container, Row, Col, Label, FormGroup, Form } from 'sveltestrap';
 </script>
 
 <style>
@@ -9,11 +7,30 @@ export let name;
 
 <main>
     <br>
-    <FormGroup>
+      <Container>
         <Input
-          type="url"
-          name="url"
-          id="exampleUrl"
-          placeholder="url placeholder" />
-      </FormGroup>
+        type="url"
+        name="url"
+        id="exampleUrl"
+        placeholder="Url" />
+        <br>
+        <div></div>
+        <br>
+        <div></div>
+        <div></div>
+
+        <br>
+        <Progress value={25} style="height: 25px;" />
+        <br>
+        <Form inline="form-inline">
+              <Label for="exampleSelect">Select</Label>
+              <Input type="select" name="select" id="exampleSelect" style="width: 30% ; margin:0px">
+                <option>1</option>
+                <option>2</option>
+                <option>3</option>
+                <option>4</option>
+                <option>5</option>
+              </Input>
+        </Form>
+      </Container>
 </main>
--- src/main.js
+++ src/main.js
@@ -4,7 +4,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 const app = new App({
     target: document.body,
     props: {
-        name: 'world'
     }
 });
 
